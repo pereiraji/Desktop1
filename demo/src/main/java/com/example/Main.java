325baef129c962dea0e7bf0f8ec5521163a22fda
José Machado <joseisaiasmachado817@gmail.com>
2023-10-18 12:21:36 -0300

    related to #1

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,24 @@
 package com.example;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.xml.sax.HandlerBase;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Conjuntos!");
+
+        Set<Integer> s1 = new HashSet<>();
+        s1.add(1);
+        s1.add(2);
+        s1.add(3);
+
+        System.out.println(s1);
+        System.out.println(s1.contains(1));
+
+        Set<Integer> empty = new HashSet<>();
+        System.out.println(s1.containsAll(empty));
+        
     }
 }
